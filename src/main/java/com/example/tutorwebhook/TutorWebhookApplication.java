55c1521bac11705c1173eb40a4f557568ee11583
Kirillmukh <Kirillmukh@mail.ru>
2023-11-15 21:42:17 +0300

    Initial commit

---
+++ src/main/java/com/example/tutorwebhook/TutorWebhookApplication.java
@@ -0,0 +1,13 @@
+package com.example.tutorwebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorWebhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutorWebhookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tutorwebhook/TutorWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tutorwebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TutorWebhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
